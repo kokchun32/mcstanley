b2ebe2a96d48be579a7a7fc38982dbe04b7ec7ef
kokchun32 <kokchun@live.co.uk>
2018-02-23 13:07:20 +0800

    migrate db booking

--- db/migrate/20180223043926_create_bookings.rb
+++ db/migrate/20180223043926_create_bookings.rb
@@ -1,6 +1,11 @@
 class CreateBookings < ActiveRecord::Migration[5.1]
   def change
     create_table :bookings do |t|
+      t.string :name, null: false
+      t.string :no_ppl, null: false
+      t.datetime :date_time, null: false
+      t.string :phone_no, null: false
+      t.string :message, null: false
 
       t.timestamps
     end
